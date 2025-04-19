cb41009bf1a6abacf8319eebeff5f2344c14e511
epistia <mateur@qq.com>
2025-04-19 15:43:00 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 # coding=utf-8
 from flask import url_for
-from markupsafe import escape
-
-from flask import Flask
+from markupsafe import escape 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return'<h1>hello Totoro</h1><img src="http://helloflask.com/totoro.gif">'
+def index():
+    return render_template('index.html',name=name,movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -23,3 +22,17 @@ def test_url_for():
     print(url_for('test_url_for'))
     print(url_for('test_url_for',num=2))
     return 'Test page'
+
+
+name = 'Gray Li'
+movies = [
+    {'title':'My Neighbor Totoro','year':'1988'},
+    {'title':'Dead Poets Society','year':'1989'},
+    {'title':'A Perfect World','year':'1993'},
+    {'title':'Leon','year':'1994'},
+    {'title':'Mahjong','year':'1996'},
+    {'title':'King of Comedy','year':'1999'},
+    {'title':'Devils on the Doorstep','year':'1999'},
+    {'title':'WALL-E','year':'2008'},
+    {'title':'The Pork of Music','year':'2012'},
+]
